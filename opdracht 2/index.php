b112070eb33b1fb24d4502833a9dd41c092b44a3
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-03-31 11:08:37 +0200

    opdracht2 finish

---
+++ opdracht 2/index.php
@@ -0,0 +1,11 @@
+<?php
+    include("variable.php");
+
+    print_r($Fruit);
+    
+    foreach($Fruit as $value) {
+        echo "<br> $value";
+    };
+
+
+?>
---
+++ opdracht 2/variable.php
@@ -0,0 +1,3 @@
+<?php
+    $Fruit = array("apple", "banana", "kiwi");
+?>
