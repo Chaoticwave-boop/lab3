f5e32d1204da453361a2196fe2129078ef10de25
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-04-08 11:25:12 +0200

    lab 3 opdracht 3 finished

---
+++ opdracht3/content.php
@@ -0,0 +1,25 @@
+
+
+ <?php 
+    $name = "Brenda";
+    $lastname = "Stoute";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="display:flex; max-width:450px">
+        <img style="width: auto; height:175px;" alt='monkey' src='melonwolf.png'>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A officia, animi unde temporibus, facilis doloribus ex doloremque soluta eum veritatis laborum natus autem quas impedit dolorum cumque quo quis numquam?
+        </p>
+    </div>
+</body>
+</html>
